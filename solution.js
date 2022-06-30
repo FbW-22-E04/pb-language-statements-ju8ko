d2250148e17a51aba4918ff10154c4ae11b12231
ju8ko <julia.x.kobzar@gmail.com>
2022-06-30 13:32:49 +0200

    The exercise is done

---
+++ solution.js
@@ -0,0 +1,10 @@
+console.log("Hello World!");
+console.log("This is my first js file");
+
+/**Remove a parenthesis from the first statement and run the code. Does your statement print?
+ * No it isn't.
+ * */
+
+/**Is there an error? If so, what kind of error is it?
+ * Yes. The error: Is there an error? SyntaxError: Unexpected string
+ * */
